83617dffbf1ed800fde81492538b2cdb0e6f678e
Jollylyrical <andersondanielramirezpardo@gmail.com>
2026-05-25 11:56:53 -0500

    HH

--- config/settings.py
+++ config/settings.py
@@ -21,7 +21,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework_simplejwt',
-    'corsheaders',
     'apps.core',
     'apps.users',
     'apps.knowledge',
@@ -39,7 +38,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
+    
 ]
 
 ROOT_URLCONF = 'config.urls'
